443269f2daf529850679ed00a488f964360c0f88
GuilhermeCarvalho <guilhermerdcarvalho@gmail.com>
2024-07-26 08:15:58 -0300

    Melhorias de nomeclarturas e organização.

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from flask import make_response
 from datetime import datetime
 
 # Fuções importadas:
-from functions import get_historical_market_data, bronze_market_data
+from functions import market_data_retrieval, market_data_transformation
 
 @functions_framework.http
 def main(request):
@@ -15,12 +15,12 @@ def main(request):
     """
     # Inicializa e executa o processo de obtenção de dados históricos
     
-    data_fetcher = get_historical_market_data.GetHistoricalData(request) # Instancia a classe GetHistoricalData
+    data_fetcher = market_data_retrieval.GetHistoricalData(request) # Instancia a classe GetHistoricalData
     fetch_response = data_fetcher.get_historical_data() # Executa a função da classe
 
     if fetch_response.status_code == 200: # Se a resposta for positiva, inicia-se o processo de transformação:
-        data_transformer = bronze_market_data.TransformMarketData(request) # Instancia a classe TransformMarketData
-        transform_response = data_transformer.transform_and_save_market_data()
+        data_transformer = market_data_transformation.TransformMarketData(request) # Instancia a classe TransformMarketData
+        transform_response = data_transformer.transform_and_store_market_data()
 
         return transform_response
     else:
